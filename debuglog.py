ddf240bdb82d21bad3f32c1f43e751f8d7a919cd
Adriano Gil <adrianogil.san@gmail.com>
2016-09-04 15:14:07 -0400

    First version printing only class name on Debug log

---
+++ debuglog.py
@@ -0,0 +1,47 @@
+import sublime, sublime_plugin
+
+import re
+
+class DebuglogCommand(sublime_plugin.TextCommand):
+	clean_name = re.compile('^\s*(public\s+|private\s+|protected\s+|static\s+|function\s+|def\s+)+', re.I)
+	def run(self, edit):
+		# self.view.insert(edit, 0, "Hello, World!")
+		view = self.view		
+
+		index = 0
+
+		for region in view.sel():
+			s = ""
+			region_row, region_col = view.rowcol(region.begin())
+			found = False
+
+			# Look for any classes
+			class_regions = view.find_by_selector('entity.name.type.class')
+			for r in reversed(class_regions):
+				row, col = view.rowcol(r.begin())
+				if row <= region_row:
+					s += view.substr(r)
+					found = True
+					break;
+
+            # Look for any functions
+			function_regions = view.find_by_selector('meta.function - meta.function.inline')
+			if function_regions:
+				for r in reversed(function_regions):
+					row, col = view.rowcol(r.begin())
+					if row <= region_row:
+						if s:
+							s += "::"
+						lines = view.substr(r).splitlines()
+						name = self.clean_name.sub('', lines[0])
+						s += name.strip()
+						found = True
+						break
+
+			s = "Debug.Log(\"" + s + "\");" 
+
+			print(str(index) + ": " + s)
+			index = index + 1
+
+			view.replace(edit, region, s)
+			
